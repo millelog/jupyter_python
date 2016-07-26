2bfa8a8ac7f08a1016f9e97b40cc30c3093852dc
millelog <millerl1960@gmail.com>
2016-07-26 14:58:21 -0700

    Removed version

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from distutils.core import setup
 setup(name='jupyter_python',
-      version='1.0',
       author='Logan Miller',
       py_modules=['create_users', 'database', 'jupyter_wrapper', 'manage_users', 'student_creation_form']
       )
