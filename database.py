aa38fe8d3954db3aece85e6042c7202d99f8bb66
millelog <millerl1960@gmail.com>
2016-08-01 10:25:39 -0700

    Changed Onid to User

--- database.py
+++ database.py
@@ -8,10 +8,10 @@ class database(object):
 	"""This class will do all read and writes to the username database"""
 	def __init__(self):
 		"""initialize the info dictionary and variables for database connection"""
-		self.info = {'first' : [], 'last' : [], 'ONID' : [], 'email' : [], 'pass' : [], 'group' : [] }
+		self.info = {'first' : [], 'last' : [], 'USER' : [], 'email' : [], 'pass' : [], 'group' : [] }
 		self.db_path = "/srv/cgrb/database.sqlite"
 		self.tn = 'users'
-		self.onid = 'ONID'
+		self.user = 'USER'
 		self.dp = 'default_pass'
 		self.g = 'group'
 		self.f = 'first'
@@ -37,15 +37,15 @@ class database(object):
 			'{f}' {tf} NOT NULL,
 			'{l}' {tf} NOT NULL,
 			'{e}' {tf} NOT NULL);'''\
-			.format(tn=self.tn, id=self.onid, tf='TEXT', dp=self.dp, gr=self.g, f=self.f, l=self.l, e=self.e))
+			.format(tn=self.tn, id=self.user, tf='TEXT', dp=self.dp, gr=self.g, f=self.f, l=self.l, e=self.e))
 		#print confimration and commit changes
 		print('Database object instantiated')
 		self.commit_db()
 
 	def log_users_creation(self):
 		with open("/srv/log.txt", "a") as log:
-			for i in range(len(self.info['ONID'])):
-				log.write(str(datetime.now())+" : added to database : "+self.info['ONID'][i]+" "+self.info['pass'][i]\
+			for i in range(len(self.info['USER'])):
+				log.write(str(datetime.now())+" : added to database : "+self.info['USER'][i]+" "+self.info['pass'][i]\
 					+" "+self.info['first'][i]+" "+self.info['last'][i]+" "+self.info['group'][i]+" "+self.info['email'][i]+"\n")
 			log.close()
 
@@ -53,12 +53,12 @@ class database(object):
 		"""Insert all of the information from the info dictionary into the sql database"""
 		self.get_connection()
 		c = self.conn.cursor()
-		#For every ONID in the dictionary
-		for i in range(len(self.info['ONID'])):
+		#For every USER in the dictionary
+		for i in range(len(self.info['USER'])):
 			#insert them into the database (I <3 SQL INJECTIONS)
-			c.execute("INSERT OR REPLACE INTO '{tn}' ('{onid}', '{dp}', '{g}', '{f}', '{l}', '{e}') VALUES ('{idv}', '{dpv}', '{gv}', '{fv}', '{lv}', '{ev}');".\
-				format(tn=self.tn, onid=self.onid, dp=self.dp, g=self.g, f=self.f, l=self.l, e=self.e,
-					idv=self.info['ONID'][i], dpv=self.info['pass'][i], gv=self.info['group'][i],
+			c.execute("INSERT OR REPLACE INTO '{tn}' ('{user}', '{dp}', '{g}', '{f}', '{l}', '{e}') VALUES ('{idv}', '{dpv}', '{gv}', '{fv}', '{lv}', '{ev}');".\
+				format(tn=self.tn, user=self.user, dp=self.dp, g=self.g, f=self.f, l=self.l, e=self.e,
+					idv=self.info['USER'][i], dpv=self.info['pass'][i], gv=self.info['group'][i],
 					 fv=self.info['first'][i], lv=self.info['last'][i], ev=self.info['email'][i]))
 		#Log this datbase manipulation
 		self.log_users_creation()
@@ -67,28 +67,28 @@ class database(object):
 
 	def set_info(self, info, group, passwds):
 		"""Set the memeber dictionary for info to the dictionary parameter"""
-		self.info = {'first' : [], 'last' : [], 'ONID' : [], 'email' : [], 'pass' : [], 'group' : []}
-		for i in range(len(info['ONID'])):
-			self.info['ONID'].append(info['ONID'][i])
+		self.info = {'first' : [], 'last' : [], 'USER' : [], 'email' : [], 'pass' : [], 'group' : []}
+		for i in range(len(info['USER'])):
+			self.info['USER'].append(info['USER'][i])
 			self.info['first'].append(info['first'][i])
 			self.info['last'].append(info['last'][i])
 			self.info['email'].append(info['email'][i])
 			self.info['pass'].append(passwds[i])
 			self.info['group'].append(group)
 
-	def remove_user(self, ONID):
-		"""Given an ONID this function will remove that student from the database"""
-		ONID = ONID.lower()
+	def remove_user(self, USER):
+		"""Given an USER this function will remove that student from the database"""
+		USER = USER.lower()
 		self.get_connection()
 		c = self.conn.cursor()
 		#sql command string
 		sql = """
 		DELETE FROM {tn}
-		WHERE {onid} = '{user}'
-		""".format(tn=self.tn, onid=self.onid, user=ONID)
+		WHERE {user} = '{user}'
+		""".format(tn=self.tn, user=self.user, user=USER)
 		#log the account deletion
 		with open("/srv/log.txt", "a") as log:
-			log.write(str(datetime.now())+" : deleted from database : "+ONID+"\n")
+			log.write(str(datetime.now())+" : deleted from database : "+USER+"\n")
 			log.close()
 		#Execute the command and commit it to the database
 		c.execute(sql)
@@ -97,25 +97,25 @@ class database(object):
 	def get_instructors(self):
 		self.get_connection()
 		c=self.conn.cursor()
-		onids = []
-		for row in c.execute("SELECT {onid} FROM {tn} WHERE {gr} = {inst}".\
-			format(onid=self.onid, tn = self.tn, gr = self.gr, inst = 'instructor')):
-				onids.append(row)
-		return onids
+		users = []
+		for row in c.execute("SELECT {user} FROM {tn} WHERE {gr} = {inst}".\
+			format(user=self.user, tn = self.tn, gr = self.gr, inst = 'instructor')):
+				users.append(row)
+		return users
 		
-	def get_onids(self):
+	def get_users(self):
 		self.get_connection()
 		c = self.conn.cursor()
-		onids = []
-		for row in c.execute("SELECT '{onid}', * FROM {tn}".format(onid=self.onid, tn=self.tn)):
-			onids.append(row)
-		return onids
+		users = []
+		for row in c.execute("SELECT '{user}', * FROM {tn}".format(user=self.user, tn=self.tn)):
+			users.append(row)
+		return users
 
 	def print_db(self):
 		self.get_connection()
 		c = self.conn.cursor()
-		for row in c.execute("SELECT {id}, * FROM {tn} ORDER BY {onid}".\
-			format(id='rowid', tn=self.tn, onid=self.onid)):
+		for row in c.execute("SELECT {id}, * FROM {tn} ORDER BY {user}".\
+			format(id='rowid', tn=self.tn, user=self.user)):
 			print(row)
 
 
