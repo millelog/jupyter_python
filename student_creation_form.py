8e13c16cc75e224c5f671659c1a1393364b81a10
millelog <millerl1960@gmail.com>
2016-08-03 20:39:18 -0700

    added input verification & debugged

--- student_creation_form.py
+++ student_creation_form.py
@@ -13,6 +13,9 @@ class student_creation_form(object):
         #Instantiate master lists of all students info
         self.info = {'first' : [], 'last' : [], 'user' : [], 'email' : []}
         self.rows = list()
+        self.submit = widgets.Button(description = "Submit Students", button_style='success')
+        self.header = widgets.HTML(value="<b>Add a new student or submit the current list of students</b>")
+        self.header.layout.margin='6px 0px 0px 50px'
         self.draw_form()
 
     def logit(self, First, Last, User, Email):
@@ -23,6 +26,7 @@ class student_creation_form(object):
                       User == row.children[2].value and\
                       Email == row.children[3].value):
                         row.layout.border='3px solid red'
+                        self.header.value="<b><font color=\"red\">Invalid character on the highlighted row</font></b>"
             else:
                 for row in self.rows:
                     if(First == row.children[0].value and\
@@ -30,19 +34,20 @@ class student_creation_form(object):
                       User == row.children[2].value and\
                       Email == row.children[3].value):
                         row.layout.border=''
+                        self.header.value="<b>Add a new student or submit the current list of students</b>"
 
     def get_user_hbox(self):
         b = widgets.HBox(width = "100%")
         boxes = list()
         y = interactive(self.logit, First = "", Last = "", User="", Email="")
-        y.children[0].layout.width = '15%'
-        y.children[0].layout.margin = '4px 30px 4px 20px'
-        y.children[1].layout.width = '15%'
-        y.children[1].layout.margin = '4px 30px 4px 20px'
+        y.children[0].layout.width = '20%'
+        #y.children[0].layout.margin = '4px 15px 4px 0px'
+        y.children[1].layout.width = '20%'
+        #y.children[1].layout.margin = '4px 15px 4px 0px'
         y.children[2].layout.width = '20%'
-        y.children[2].layout.margin = '4px 30px 4px 20px'
+        #y.children[2].layout.margin = '4px 15px 4px 0px'
         y.children[3].layout.width = '40%'
-        y.children[3].layout.margin = '4px 30px 4px 20px'
+        #y.children[3].layout.margin = '4px 15px 4px 0px'
         boxes.extend(y.children)
 
         b.children = [b for b in boxes]
@@ -51,16 +56,20 @@ class student_creation_form(object):
     def new_row(self, b):
         newrow = self.get_user_hbox()
         self.rows.append(newrow)
+        if len(self.rows)>1:
+            self.submit.close()
         display(newrow)
+        self.submit = widgets.Button(description = "Submit Students", button_style='success')
+        display(self.submit)
+        self.submit.on_click(self.on_submit_clicked)
 
 
     def draw_form(self):
         bt = widgets.Button(description = "New Student", button_style='primary')
-        submit = widgets.Button(description = "Submit Students", button_style='success')
-        submit.on_click(self.on_submit_clicked)
+        #submit = widgets.Button(description = "Submit Students", button_style='success')
         bt.on_click(self.new_row)
-        buttons = widgets.HBox(children=[bt, submit])
-        display(buttons)
+        top = widgets.HBox(children=[bt, self.header])
+        display(top)
 
     #Input validator to protect from sql injection
     def valid_input(self, input_string):
@@ -74,7 +83,7 @@ class student_creation_form(object):
     def add_users(self, info):
         #instantiate database object
         db = users.create_database()
-        print('Working... may take several minutes...')
+        print('Working...')
         #Initalize Email server
         smtpserver = create.initialize_smtp_server('mail.engr.oregonstate.edu', 25, 'millelog', 'F1c2g3d4b5a')
 
@@ -90,7 +99,7 @@ class student_creation_form(object):
         print("All students created succesfully")
 
     def valid_form(self):
-        for row in self.form.rows:
+        for row in self.rows:
             if(not (self.valid_input(row.children[0].value) and\
               self.valid_input(row.children[1].value) and\
               self.valid_input(row.children[2].value) and\
@@ -103,12 +112,11 @@ class student_creation_form(object):
     
     def on_submit_clicked(self, b):
         if(self.valid_form()):
-            for row in self.form.rows:
+            for row in self.rows:
                 self.info['first'].append(row.children[0].value)
                 self.info['last'].append(row.children[1].value)
                 self.info['user'].append(row.children[2].value)  
                 self.info['email'].append(row.children[3].value)
-                print("Form is properly formatted")
             self.add_users(self.info)
         else:
-            print("Invalid character(s) used in the highlighted student")
+            print("Invalid character(s) submitted")
